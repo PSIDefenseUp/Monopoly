50ec41f1486de6288a83aae04f9df95740c1cbc2
ncangeles <nangel6@uic.edu>
2015-10-11 12:55:33 -0500

    finished getRent() should be done?
    used same getPossibleActions() and onLand()

--- Utility.java
+++ Utility.java
@@ -24,24 +24,62 @@ public class Utility extends Property
     
 ///////////////////////////////////////////////////////////////////////////////////////////////////
     public int getRent()
-    // POST: FCTVAL == rent
+    // PRE:  1 <= diceRoll <= 12 diceRoll is amount player rolled owner = Player who owns Utility
+    //       assumes player has at least 1 utility and player can only have up to 2 utility
+    // POST: FCTVAL == rent player owes 
     {
-        // TODO: get how much should be paid when landing on a utility depending on what the owner has
-        return 0; // don't know the algorithm
+        int numberOwned = 0;    // keeps track of how many utility is owned
+        Property[] property = owner.getProperties();  // properties owned by the player
+        
+        for (int i = 0; i < property.length; i++)   // counts how many utility is owned
+        {
+            if (property[i] instanceof Utility)
+            {
+                numberOwned++;
+            }            
+        }
+        
+        if (numberOwned == 2)
+        {
+            return owner.getRoll() * 10;
+        }
+        else
+        {
+            return owner.getRoll() * 4;
+        }
     }
         
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-    public void onLand()
+    public void onLand(Player player)
     // POST: see below
     {
-        // if unowned
-          // if player has enough money
-            // offer purchase
+        int option;  // option the player chooses
         
-        // else if owned <by other player> 
-          // if player has enough funds
-             // give owner funds
-          // else player doesn't have enough
-             // give owner all player's money
+        option = ActionsMenu.runActionsMenu(getPossibleActions(player));  // ask player for option
+        
+        switch(option)  // take appropriate actions 
+        {
+            case 1:  player.changeMoney(-1 * super.getCost());  // buy utility
+                     super.setOwner(player);
+                     break;
+            default: player.changeMoney(-1 * getRent());  // use utility
+                     owner.changeMoney(getRent());
+        }
+        
+        return;
+    }
+    
+     public String[] getPossibleActions(Player player)
+    // PRE:  player is initialized
+    // POST: FCTVAL = array of options player has upon landing on this space, 
+    //       to be used in a menu in a user interface
+    {
+        String[] notOwned = {"End Turn", "Buy"};
+        String[] owned = {"Pay Rent"};
+        
+        if(owner == null)  // if the railroad isn't owned,
+            return notOwned;
+        else  // else it must be owned
+            return owned;
     }
 }
