0f47907a1ddbd4187b5ec60311653f80a21fa649
ncangeles <nangel6@uic.edu>
2015-10-12 13:02:17 -0500

    changed move() slightly
    used 40 since gives me an error when doing testdriver for methods. feel free to make it a constant or change it back since for the deliverable we do not need to include Monopoly class.

--- Player.java
+++ Player.java
@@ -70,9 +70,9 @@ public class Player
     // POST: moves player forward "moves" spaces
     {
         position += moves;
-        if(position >= Monopoly.getBoardLength())  // if positions moved outside of the array,
+        if(position >= 40)  // if positions moved outside of the array,
         {
-            position %= Monopoly.getBoardLength();  // put it back on the board
+            position %= 40;  // put it back on the board
 
             money += 200;  // add $200
         }
