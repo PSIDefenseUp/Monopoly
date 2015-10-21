22fbff0d1e11b8eb77da6ff89bf28a77aaba8b04
ncangeles <nangel6@uic.edu>
2015-10-21 17:08:38 -0500

    finished

--- Player.java
+++ Player.java
@@ -3,6 +3,7 @@
 // Date: October 2015        
 // Description: Class to represent the players in a Monopoly game
 
+import javax.swing.JOptionPane;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
@@ -16,7 +17,7 @@ public class Player
     private Property[] properties;          // List of properties owned by this player
     private String name;                    // name of the player
     private BufferedImage token;            // the token the player appears as
-    
+
     public Player()
     // POST: Creates Player instance with money = $1500, position = 0, no properties,
     //        and a name set to "Not Named"
@@ -26,7 +27,7 @@ public class Player
         position = 0;
         properties = new Property[0];
     }
-    
+
     public Player(String name)
     // PRE:  name must be initialized
     // POST: Creates Player instance with money = $1500, position = 0, no properties,
@@ -37,25 +38,25 @@ public class Player
         position = 0;
         properties = new Property[0];
     }
-    
+
     public int getMoney()
     // POST: FCTVAL == money
     {
         return money;
     }
-    
+
     public Property[] getProperties()
     // POST: FCTVAL == properties
     {
         return properties;
     }
-    
+
     public int getPosition()
     // POST: FCTVAL == position
     {
         return position;
     }
-    
+
     public void move(int moves)
     // PRE:  moves > 0
     // POST: moves player forward "moves" spaces
@@ -67,44 +68,44 @@ public class Player
             money += 200;  // add $200
         }
     }
-    
+
     public void moveTo(int position)
     // PRE:  0 <= position < 40
     // POST: player's position == position
     {
         this.position = position;
     }
-    
+
     public void changeMoney(int money)
     // PRE:  money is initialized
     // POST: players money is changed by "money" 
     {
         this.money += money;            
     }
-    
+
     public void addProperty(Property property)
     // PRE:  property is initialized
     // POST: property is added to instances' property list
     {        
         Property[] arr;  // new list of properties for the player
         int i;  // iterator for array manipulation
-        
+
         property.setOwner(this);
         arr = new Property[properties.length + 1];  // initialize list for one more property
         i = properties.length;
-        
+
         if(property instanceof Lot)  // if property is a Lot
         {
             // while the end is a Railroad or Utility, move ahead
             while((i > 0) && 
-                  ((properties[i-1] instanceof Utility) || (properties[i-1] instanceof Railroad)) )
+            ((properties[i-1] instanceof Utility) || (properties[i-1] instanceof Railroad)) )
             {
                 arr[i] = properties[i-1];
                 --i;
             }
             // while the end is a larger position, move ahead
             while((i > 0) && 
-                  (properties[i-1].getPosition() > property.getPosition()))
+            (properties[i-1].getPosition() > property.getPosition()))
             {
                 arr[i] = properties[i-1];
                 --i;
@@ -114,15 +115,15 @@ public class Player
         {
             // while the end is a Utility, move ahead
             while((i > 0) && 
-                  (properties[i-1] instanceof Utility))
+            (properties[i-1] instanceof Utility))
             {
                 arr[i] = properties[i-1];
                 --i;
             }
             // while the end is a Railroad and is a larger position, move ahead
             while((i > 0) && 
-                  (properties[i-1] instanceof Railroad) &&
-                  (properties[i-1].getPosition() > property.getPosition()))
+            (properties[i-1] instanceof Railroad) &&
+            (properties[i-1].getPosition() > property.getPosition()))
             {
                 arr[i] = properties[i-1];
                 --i;
@@ -132,27 +133,27 @@ public class Player
         {
             // while the end is a Utility and is a larger postion, move ahead
             while((i > 0) && 
-                  (properties[i-1] instanceof Utility) &&
-                  (properties[i-1].getPosition() > property.getPosition()))
+            (properties[i-1] instanceof Utility) &&
+            (properties[i-1].getPosition() > property.getPosition()))
             {
                 arr[i] = properties[i-1];
                 --i;
             }
         }
-        
+
         arr[i] = property;  // put property where it belongs
         --i;
-       
+
         // copy over the rest of the array
         while(i >= 0)
         {
-                arr[i] = properties[i];
-                --i;
+            arr[i] = properties[i];
+            --i;
         }
 
         properties = arr;  // point players properties to new list
     }
-    
+
     public void removeProperty(Property property)
     // PRE:  property is initialized
     // POST: property is removed from instances' property list and its owner is set to null
@@ -163,15 +164,15 @@ public class Player
         // Don't try to remove properties that this player doesn't own
         if(property.getOwner() != this)
             return;
-        
+
         property.setOwner(null); // reset the owner of the property
         arr = new Property[properties.length - 1];  // initialize list for one less property
-        
+
         j = 0;
         for (int i = 0; i < properties.length; i++) // copies original properties into new list
         {
             if (property.getName() != properties[i].getName())  // if current instance isn't the 
-                                                                // the one we're searching for
+            // the one we're searching for
             {
                 arr[j] = properties[i];  // copy it
                 j++;
@@ -179,19 +180,19 @@ public class Player
         }
         properties = arr;  // point players properties to new list
     }
-    
+
     public String getName()
     // POST: FCTVAL == name of the player
     {
         return name;
     }
-    
+
     public String toString()
     // POST: string representation of money, position, and properties
     {
         String s = "Name: " + name + ", Money: " + money + " Postition: " + position;
         s = s + " Properties: ";
-        
+
         for (int i = 0; i < properties.length; i++)
         {
             s = s + properties[i].getName();
@@ -256,7 +257,7 @@ public class Player
             g.drawRect(x, y, width, height);    
         }
     }
-    
+
     public Lot[] getUpgradeableLots(Property[] properties)
     // PRE:  properties is intialized, and owned by a single owner
     // POST: FCTVAL a list of properties <owned by player> that can be upgraded
@@ -267,13 +268,13 @@ public class Player
         int j;              // iterator for the color
         int max;            // maximum upgarde count for color-group
         int min;            // minimum upgarde count for color-group
-        
+
         lots = new Property[22];
         i = 0;
         j = 0;
         min = 0;
         max = 0;
-        
+
         for(Property p : properties)
         {
             if(p instanceof Lot)  // if instance is a Lot
@@ -311,7 +312,7 @@ public class Player
                             lots[i] = p;  // add to lots
                             ++i;  // incriment pointers
                         }
-                        
+
                         // check if group has 3 or 2 members
                         if ((p.getPosition() > 3) && (p.getPosition() < 37))  // if a group of 3
                             ++j;  // let it search for the third color
@@ -357,15 +358,15 @@ public class Player
                             lots[i] = p;  // put on lots
                             ++i;  // increase lots size
                         }
-                        
+
                         if((min == 5) && (i > 0)) // and group is fully upgraded, and still on lots
+                        {
+                            do  //remove them from lots
                             {
-                                do  //remove them from lots
-                                {
-                                    --i;  
-                                }
-                                while((i > 0) && (((Lot)lots[i]).getUpgradeCount() == 5));
+                                --i;  
                             }
+                            while((i > 0) && (((Lot)lots[i]).getUpgradeCount() == 5));
+                        }
                         j = 0;  // start search for new color
                     }
                     else  // if color didn't match prev
@@ -386,50 +387,60 @@ public class Player
                 }
             }
         }
-        
+
         if(j == 1)  // if the last lot onto lots wasn't complete group
             --i;
         else if(j==2)  // if last lot onot lots wasn't complete group
             i -= 2;
-        
+
         retVal = new Lot[i]; // create an exact size return array
         for(j=0 ; j<i ; ++j)  // copy values over
         {
             retVal[j] = (Lot)lots[j];
         }
-        
+
         return retVal;
     }
-    
+
     public void upgrade()
     // PRE:  player must be willing to upgrade his lot(s)
     // POST: 
     {
         Lot[] lots;  // the lots the play can upgrade
-        int option;  // variable for choosing what to do
         String[] options;  // string of options
+        int option; // the option chosen by the user
+        String optionString;
+
+        lots = this.getUpgradeableLots(properties);
+        options = new String[lots.length];
+        option = -1;
         
-        do
+        if (options.length != 0)
         {
-            lots = getUpgradeableLots(this.getProperties());  // get a list of possible choice
-            options = new String[lots.length + 1];  // get array of possible upgardes
-            options[0] = "Return";
-            for(int i=1 ; i<=lots.length ; ++i)  // add upgardes to options array
+            for(int i = 0; i < options.length; i++)
             {
-                options[i] = "Upgrade " + lots[i-1].getName();
+                options[i] = lots[i].getName() + " - $" + lots[i].getUpgradeCost();
             }
-            
-            option = ActionsMenu.runActionsMenu(options);  // ask user for upgrade
-            
-            if(option != 0) // if player chose to upgrade
+    
+            optionString = (String)JOptionPane.showInputDialog(null, "Select a property to upgrade", "UPGRADE", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+    
+            if(optionString != null)
             {
-                this.changeMoney(-1 * lots[option-1].getUpgradeCost());  // take their money
-                lots[option-1].upgrade();
+                for(int i = 0; i < lots.length; i++)
+                {
+                    String name = optionString.substring(0, lots[i].getName().length());
+                    if(name.equals(lots[i].getName()))
+                    {
+                        option = i;
+                    }
+                }
+    
+                this.changeMoney(-1 * lots[option].getUpgradeCost());  // take their money
+                lots[option].upgrade();
             }
         }
-        while(option != 0);
     }
-    
+
     public Lot[] getDowngradeable(Property[] sellable)
     // PRE:  
     // POST: 
@@ -443,15 +454,15 @@ public class Player
         int iS;  // iterator for the <incoming> sellable array
         int max;  // max and min upgrade counts
         int min;
-        
+
         temp1 = new Property[this.getProperties().length];
         iT1 = 0;
         iS = 0;
-            
+
         //if the players property isn't sellable, it must be downgradeable
         for(Property p : this.getProperties())
         {
-            if(sellable[iS].getName() == p.getName())  // if an element of the sellable array, skip it
+            if((sellable.length != 0) && sellable[iS].getName() == p.getName())  // if an element of the sellable array, skip it
             {
                 ++iS;
             }
@@ -463,14 +474,14 @@ public class Player
                     ++iT1;
                 }
             }
-        }
-        
+       }
+
         temp2 = new Property[iT1];  // create 2nd temp array
         iT2 = 0;  // initialize needed elements
         min = 0;
         max = 0;
         color = Color.WHITE;  // set 
-        
+
         for(int i=0 ; i<iT1 ; ++i)
         {
             if( !(color.equals(temp1[i].getColor())) )  // if we're in a new group
@@ -502,17 +513,17 @@ public class Player
             }
             // else the element must have too high of an upgrade count (so don't include it)
         }
-        
+
         retVal = new Lot[iT2];  // create the return array
-        
+
         for(int j=0 ; j<iT2 ; ++j)  // fill the return array
         {
             retVal[j] = (Lot)temp2[j];
         }
-        
+
         return retVal;
     }
-    
+
     public Property[] getSellable()
     // PRE:  
     // POST: 
@@ -521,11 +532,11 @@ public class Player
         Property[] retVal;  // array to return
         Color bad;  // color of the group that isn't to be saved
         int i; // iterater for an array
-        
+
         temp = new Property[this.getProperties().length];
         i = 0;
         bad = Color.WHITE;
-        
+
         for(Property p : this.getProperties())  // go through players properties
         {
             if( !(p instanceof Lot) ) // if the property isn't a lot, include it
@@ -544,86 +555,93 @@ public class Player
             else  // else it's upgraded, 
             {
                 bad = p.getColor();  // save off the color of the upgraded 
-                
+
                 while(i > 0 && bad.equals(temp[i].getColor()))  // remove any lots of the same color
                 {
                     --i;
                 }
             }
         }
-        
+
         retVal = new Property[i];  // create an exact sized array
         for(int j=0 ; j<i ; ++j)  // copy over properties
         {
             retVal[j] = temp[j];
         }
-        
+
         return retVal;
     }
-        
+
     public void sell()
     // PRE:  
     // POST: 
     {
-        Property[] sellable;  // array for sellable properties
-        Lot[] downgradeable;  // array for downgradeable properties
         String[] options;  // array of options
-        int option;  // flag that shows which option the user chose
-        
+        Property[] sellable;
+        Lot[] downgradeable;
+        int option = 0;
+        String optionString;
+
         sellable = getSellable();
         downgradeable = getDowngradeable(sellable);
-        
-        do
+
+        options = new String[] {"Sell Improvements", "Sell Properties"};
+        option = JOptionPane.showOptionDialog(null, "Choose what to sell", "SELLING",JOptionPane.DEFAULT_OPTION,JOptionPane.QUESTION_MESSAGE,null, options, options[0]);
+        if(option == 0)
         {
-            options = new String[] {"Return", "Sell Improvements", "Sell Properties"};
-            option = ActionsMenu.runActionsMenu(options);
-            
-            if(option == 0)
-                return;
-            else if(option == 1)
+            options = new String[downgradeable.length];
+            if (options.length != 0)
             {
-                do
+                for(int i=0 ; i<downgradeable.length ; ++i)
                 {
-                    options = new String[downgradeable.length + 1];
-                    options[0] = "Return";
-                    for(int i=0 ; i<downgradeable.length ; ++i)
-                    {
-                        options[i+1] = "Downgrade " + downgradeable[i].getName();
-                    }
-                    option = ActionsMenu.runActionsMenu(options);
-                    if( option != 0)
-                    {
-                        downgradeable[option-1].downgrade();
-                        this.changeMoney(downgradeable[option-1].getUpgradeCost()/2);
-                        sellable = getSellable();
-                        downgradeable = getDowngradeable(sellable);
-                    }
+                    options[i] = downgradeable[i].getName();
                 }
-                while(option != 0);
-            }
-            else if (option == 2)
-            {
-                do
+                optionString = (String)JOptionPane.showInputDialog(null, "Select a property to downgrade", "SELlING UPGRADES", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+                if(optionString != null)
                 {
-                    options = new String[sellable.length + 1];
-                    options[0] = "Return";
-                    for(int i=0 ; i<sellable.length ; ++i)
+                    for(int i = 0; i < downgradeable.length; i++)
                     {
-                        options[i+1] = "Sell " + sellable[i].getName();
+                        String name = optionString.substring(0, downgradeable[i].getName().length());
+                        if(name.equals(downgradeable[i].getName()))
+                        {
+                            option = i;
+                        }
                     }
-                    option = ActionsMenu.runActionsMenu(options);
-                    if( option != 0)
-                    {
-                        this.changeMoney(sellable[option-1].getCost());
-                        this.removeProperty(sellable[option-1]);
-                        sellable[option-1].setOwner(null);
-                        sellable = getSellable();
-                    }
+    
+                    downgradeable[option].downgrade();
+                    this.changeMoney(downgradeable[option].getUpgradeCost()/2);
+                    sellable = getSellable();
+                    downgradeable = getDowngradeable(sellable);
                 }
-                while(option != 0);
             }
         }
-        while(true);
+        else if (option == 1)
+        {
+            options = new String[sellable.length];
+            if (options.length != 0)
+            {
+                for(int i=0 ; i<sellable.length ; ++i)
+                {
+                    options[i] = sellable[i].getName();
+                }
+                optionString = (String)JOptionPane.showInputDialog(null, "Select a property to sell", "SELlING PROPERTIES", JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+                if(optionString != null)
+                {
+                    for(int i = 0; i < sellable.length; i++)
+                    {
+                        String name = optionString.substring(0, sellable[i].getName().length());
+                        if(name.equals(sellable[i].getName()))
+                        {
+                            option = i;
+                        }
+                    }
+                    this.changeMoney(sellable[option].getCost());
+                    this.removeProperty(sellable[option]);
+                    sellable[option].setOwner(null);
+                    sellable = getSellable();
+                }
+            }
+        }
     }
 
     public void bankrupt(Player other)
@@ -632,7 +650,7 @@ public class Player
     //       all of their money is handed over to other, and their money is set to 0
     {
         int money;  // self explainatory
-                
+
         for(Property p : properties) // hand players properties over to owner
         {
             if(other != null)
