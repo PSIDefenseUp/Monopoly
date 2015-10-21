0f70e357b1339819669704bb6082ce328525560c
ncangeles <nangel6@uic.edu>
2015-10-21 17:07:32 -0500

    finished

--- Monopoly.java
+++ Monopoly.java
@@ -24,34 +24,34 @@ public class Monopoly extends JApplet implements ActionListener
     private boolean upgrade;          // is upgrading
 
     public static final int UIPADDING = 150; // Padding for upper UI area
-    
+
     public void init()
     // POST: Initializes all game elements, such as the board and players in the game
     {
         setLayout(new FlowLayout());
-        
+
         // Set up board
         setBoard();
-        
+
         // Initialize players
         players = new Player[4];
         for (int i = 0; i < players.length; i++)
         {
-             players[i] = new Player("Player " + (i + 1));
+            players[i] = new Player("Player " + (i + 1));
         }
-        
+
         // Adding UI elements
         showProperty = new JButton("Show Player Properties");
         showProperty.addActionListener(this);
-        
+
         gameMode = new JButton[2];
         gameMode[0] = new JButton("Start New Game");
         gameMode[1] = new JButton("Demo Mode");
         gameMode[0].addActionListener(this);
         gameMode[1].addActionListener(this);
-        
+
         panel = new JPanel();
-        
+
         // Adding UI
         add(gameMode[0]);
         add(gameMode[1]);
@@ -60,7 +60,7 @@ public class Monopoly extends JApplet implements ActionListener
 
         //play();
     }
-    
+
     @Override
     public void paint(Graphics g)
     {
@@ -71,7 +71,7 @@ public class Monopoly extends JApplet implements ActionListener
         int i = g.getFontMetrics().stringWidth(potString);
         g.drawString(potString, (getWidth()/2) - i/2, getHeight()/2);
     }
-    
+
     public void addButtons(String[] options)
     // PRE:  options = string[] of options to be turned into buttons
     // POST: replace previous buttons and puts new buttons
@@ -117,7 +117,7 @@ public class Monopoly extends JApplet implements ActionListener
             x += width + padding;
         }
     }
-    
+
     public void drawBoard(Graphics g)
     //PRE:  Applet size >= 600x550 must be a square with +50 in height for buttons
     //      g = object to draw on
@@ -129,7 +129,7 @@ public class Monopoly extends JApplet implements ActionListener
         int height = getHeight() - startY;    // height of board        
         int tileWidth = width/11;             // width of each tile
         int tileHeight = height/11;           // height of each tile
-        
+
         for (int i = 0; i < 40; i++)
         {
             board[i].render(g, startX, startY, tileWidth, tileHeight);
@@ -152,89 +152,89 @@ public class Monopoly extends JApplet implements ActionListener
             }
         }
     }
-    
+
     public static void setBoard()
     {   
         Color purple; // the color purple
-            
+
         purple = new Color(102, 51, 153);  // Initialize the color of purple
         board = new BoardLoc[40];  // Initialize array of board locations
-                
+
         // Implement board spaces      
         board[0] = new CornerSquare("GO", 0);
         board[1] = new Lot("MEDITERRANEAN AVE", 1, 60, purple, 
-                           50, new int[]{2, 10, 30, 90, 160, 230});
+            50, new int[]{2, 10, 30, 90, 160, 230});
         board[2] = new CardSquare("Community Chest", 2);
         board[3] = new Lot("BALTIC AVE", 3, 60, purple, 
-                           50, new int[]{4, 20, 60, 180, 320, 460});
+            50, new int[]{4, 20, 60, 180, 320, 460});
         board[4] = new TaxSquare("Income Tax", 4);
         board[5] = new Railroad("READING RAILROAD", 5, 200);
         board[6] = new Lot("ORIENTAL AVE.", 6, 100, Color.CYAN,
-                           50, new int[]{6, 30, 90, 270, 400, 550});
+            50, new int[]{6, 30, 90, 270, 400, 550});
         board[7] = new CardSquare("Chance", 7);
         board[8] = new Lot("VERMONT AVE.", 8, 100, Color.CYAN, 
-                           50, new int[]{6, 30, 90, 270, 400, 550});
+            50, new int[]{6, 30, 90, 270, 400, 550});
         board[9] = new Lot("CONNECTICUT AVE.", 9, 120, Color.CYAN, 
-                           50, new int[]{8, 40, 100, 300, 450, 600});
+            50, new int[]{8, 40, 100, 300, 450, 600});
         board[10] = new CornerSquare("Jail", 10);
         board[11] = new Lot("ST. CHARLES PLACE", 11, 140, Color.MAGENTA, 
-                            100, new int[]{10, 50, 150, 450, 625, 750});
+            100, new int[]{10, 50, 150, 450, 625, 750});
         board[12] = new Utility("ELECTRIC COMPANY", 12, 150);
         board[13] = new Lot("STATES AVE.", 13, 140, Color.MAGENTA, 
-                            100, new int[]{10, 50, 150, 450, 625, 750});
+            100, new int[]{10, 50, 150, 450, 625, 750});
         board[14] = new Lot("VIRGINIA AVE.", 14, 160, Color.MAGENTA, 
-                            100, new int[]{12, 60, 180, 500, 700, 900});
+            100, new int[]{12, 60, 180, 500, 700, 900});
         board[15] = new Railroad("PENNSYLVANIA RAILROAD", 15, 200);
         board[16] = new Lot("ST. JAMES PLACE", 16, 180, Color.ORANGE, 
-                            100, new int[]{14, 70, 200, 550, 750, 950});
+            100, new int[]{14, 70, 200, 550, 750, 950});
         board[17] = new CardSquare("Community Chest", 17);
         board[18] = new Lot("TENNESSEE AVE.", 18, 180, Color.ORANGE, 
-                            100, new int[]{14, 70, 200, 550, 750, 950});
+            100, new int[]{14, 70, 200, 550, 750, 950});
         board[19] = new Lot("NEW YORK AVE.", 19, 200, Color.ORANGE, 
-                            100, new int[]{16, 80, 220, 600, 800, 1000});
+            100, new int[]{16, 80, 220, 600, 800, 1000});
         board[20] = new CornerSquare("Free Parking", 20);
         board[21] = new Lot("KENTUCKY AVE.", 21, 220, Color.RED, 
-                            150, new int[]{18, 90, 250, 700, 875, 1050});
+            150, new int[]{18, 90, 250, 700, 875, 1050});
         board[22] = new CardSquare("Chance", 22);
         board[23] = new Lot("INDIANA AVE.", 23, 220, Color.RED, 
-                            150, new int[]{18, 90, 250, 700, 875, 1050});
+            150, new int[]{18, 90, 250, 700, 875, 1050});
         board[24] = new Lot("ILLINOIS AVE.", 24, 240, Color.RED, 
-                            150, new int[]{20, 100, 300, 750, 925, 1100});
+            150, new int[]{20, 100, 300, 750, 925, 1100});
         board[25] = new Railroad("B & O RAILROAD", 25, 200);
         board[26] = new Lot("ATLANTIC AVE.", 26, 260, Color.YELLOW, 
-                            150, new int[]{22, 110, 330, 800, 975, 1150});
+            150, new int[]{22, 110, 330, 800, 975, 1150});
         board[27] = new Lot("VENTNOR AVE.", 27, 260, Color.YELLOW, 
-                            150, new int[]{22, 110, 330, 800, 975, 1150});
+            150, new int[]{22, 110, 330, 800, 975, 1150});
         board[28] = new Utility("WATER WORKS", 28, 150);
         board[29] = new Lot("MARVIN GARDENS", 29, 280, Color.YELLOW, 
-                            150, new int[]{24, 120, 360, 850, 1025, 1200});
+            150, new int[]{24, 120, 360, 850, 1025, 1200});
         board[30] = new CornerSquare("Go To Jail", 30);
         board[31] = new Lot("PACIFIC AVE.", 31, 300, Color.GREEN, 
-                            200, new int[]{26, 130, 390, 900, 1100, 1275});
+            200, new int[]{26, 130, 390, 900, 1100, 1275});
         board[32] = new Lot("NO. CAROLINA AVE.", 32, 300, Color.GREEN, 
-                            200, new int[]{26, 130, 390, 900, 1100, 1275});
+            200, new int[]{26, 130, 390, 900, 1100, 1275});
         board[33] = new CardSquare("Community Chest", 33);
         board[34] = new Lot("PENNSYLVANIA AVE.", 34, 320, Color.GREEN, 
-                            200, new int[]{28, 150, 450, 1000, 1200, 1400});
+            200, new int[]{28, 150, 450, 1000, 1200, 1400});
         board[35] = new Railroad("SHORT LINE RAILROAD", 35, 200);
         board[36] = new CardSquare("Chance", 36);
         board[37] = new Lot("PARK PLACE", 37, 350, Color.BLUE, 
-                            200, new int[]{35, 175, 500, 1100, 1300, 1500});
+            200, new int[]{35, 175, 500, 1100, 1300, 1500});
         board[38] = new TaxSquare("Luxury Tax", 38);
         board[39] = new Lot("BOARDWALK", 39, 400, Color.BLUE, 
-                            200, new int[]{50, 200, 600, 1400, 1700, 2000});
+            200, new int[]{50, 200, 600, 1400, 1700, 2000});
     }
-    
+
     public void play()
     {
-            // If current player is still in the game
-            //     Let current player take an action or roll
-            //     After roll, resolve landing on the player's new location
-            //     ?? Check if player is bankrupt ??
-            // Increment currentPlayer to progress to next player's turn
-           // System.out.println("Player " + (currentPlayer+1) + "'s turn.");
-          //  System.out.println(players[currentPlayer].toString());
-            
+        // If current player is still in the game
+        //     Let current player take an action or roll
+        //     After roll, resolve landing on the player's new location
+        //     ?? Check if player is bankrupt ??
+        // Increment currentPlayer to progress to next player's turn
+        // System.out.println("Player " + (currentPlayer+1) + "'s turn.");
+        //  System.out.println(players[currentPlayer].toString());
+
         if(players[currentPlayer].getMoney() > 0) // if player is still in game
         {   
             addButtons(new String[] {"Roll dice", "Upgrade a property", "Sell a property"});              
@@ -270,7 +270,7 @@ public class Monopoly extends JApplet implements ActionListener
         rolled = false;
         play();
     }
-    
+
     public void actionPerformed(ActionEvent e)
     {
         if(e.getSource() == gameMode[0])    // Start button lets players choose tokens and amount of players with error checking
@@ -288,17 +288,17 @@ public class Monopoly extends JApplet implements ActionListener
             players = new Player[num];
             for (int i = 0; i < num; i++)
             {
-                 int temp;
-                 players[i] = new Player("Player " + (i + 1));
-                 do {
-                     s = JOptionPane.showInputDialog("Pick token for Player "+ (i+1) +" : (Between 1 to 4)");
-                     temp = Integer.parseInt(s);
-                     s = "token" + s + ".png";
-                     players[i].setToken(s);
-                 }while (temp > 4 || temp < 1);
+                int temp;
+                players[i] = new Player("Player " + (i + 1));
+                do {
+                    s = JOptionPane.showInputDialog("Pick token for Player "+ (i+1) +" : (Between 1 to 4)");
+                    temp = Integer.parseInt(s);
+                    s = "token" + s + ".png";
+                    players[i].setToken(s);
+                }while (temp > 4 || temp < 1);
             }
             // Prepare game for start
-            currentPlayer = 0;
+            currentPlayer = (int)Math.random()*players.length;
             gameOver = false;
             roll = 1;
             play();
@@ -309,18 +309,33 @@ public class Monopoly extends JApplet implements ActionListener
             remove(gameMode[1]);
             add(showProperty);
             // Initialize players
-            players = new Player[4];
+            players = new Player[2];
             for (int i = 0; i < players.length; i++)
             {
-                 players[i] = new Player("Player " + (i + 1));
+                players[i] = new Player("Player " + (i + 1));
             }
             players[0].setToken("token1.png");
             players[1].setToken("token2.png");
-            players[2].setToken("token3.png");
-            players[3].setToken("token4.png");
+            
             players[0].addProperty((Property)board[1]);
             players[0].addProperty((Property)board[3]);
-             // Prepare game for start
+            players[0].addProperty((Property)board[5]);
+            players[0].addProperty((Property)board[6]);
+            players[0].addProperty((Property)board[8]);
+            players[0].addProperty((Property)board[9]);
+            players[0].addProperty((Property)board[11]);
+            players[0].addProperty((Property)board[12]);
+            
+            players[1].addProperty((Property)board[13]);
+            players[1].addProperty((Property)board[14]);
+            players[1].addProperty((Property)board[15]);
+            players[1].addProperty((Property)board[16]);
+            players[1].addProperty((Property)board[18]);
+            players[1].addProperty((Property)board[19]);
+            players[1].addProperty((Property)board[21]);
+            players[1].addProperty((Property)board[23]);
+
+            // Prepare game for start
             currentPlayer = 0;
             gameOver = false;
             roll = 1;
@@ -386,11 +401,11 @@ public class Monopoly extends JApplet implements ActionListener
                     switch(i)
                     {
                         case 0: rolled = true; rollDice(); 
-                                break;
+                        break;
                         case 1: players[currentPlayer].upgrade(); 
-                                break;
+                        break;
                         case 2: players[currentPlayer].sell(); 
-                                break;
+                        break;
                         default: break;
                     }                    
                 }                
@@ -408,7 +423,7 @@ public class Monopoly extends JApplet implements ActionListener
         }        
         repaint();
     }    
-    
+
     public static Player getCurrentPlayer()
     // POST: FCTVAL = Current Player instance
     {
